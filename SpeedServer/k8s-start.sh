f143be880a7b2c07d7b1c75378753ca43ac71b48
Nicholas Arduini <nicholas.arduini@bell.ca>
2018-06-04 12:36:43 -0400

    remove SpeedServer

--- SpeedServer/k8s-start.sh
+++
@@ -1,7 +0,0 @@
-./mvnw -DskipTests package
-
-docker build -t gcr.io/axiomatic-lamp-176915/speed:v4 . 
-
-gcloud docker -- push gcr.io/axiomatic-lamp-176915/speed:v4
-
-kubectl set image deployment/speed speed=gcr.io/axiomatic-lamp-176915/speed:v4
