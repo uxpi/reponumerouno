d68c007de9eb081df0d2ffaf58aa582f67c1c83d
Nicholas Arduini <nicholas.arduini@bell.ca>
2018-06-04 11:43:10 -0400

    changed min file upload size

--- SpeedServer/k8s-start.sh
+++ SpeedServer/k8s-start.sh
@@ -1,7 +1,7 @@
 ./mvnw -DskipTests package
 
-docker build -t gcr.io/axiomatic-lamp-176915/speed:v3 . 
+docker build -t gcr.io/axiomatic-lamp-176915/speed:v4 . 
 
-gcloud docker -- push gcr.io/axiomatic-lamp-176915/speed:v3
+gcloud docker -- push gcr.io/axiomatic-lamp-176915/speed:v4
 
-kubectl set image deployment/speed speed=gcr.io/axiomatic-lamp-176915/speed:v3
+kubectl set image deployment/speed speed=gcr.io/axiomatic-lamp-176915/speed:v4
